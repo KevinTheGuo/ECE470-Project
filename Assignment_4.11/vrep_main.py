84d8b119c59ee02394e9fbbffcf1104e380ad603
kthayyil <kevin.thayyil@gmail.com>
2018-04-11 22:14:45 -0500

    wall building

--- Assignment_4.11/vrep_main.py
+++ Assignment_4.11/vrep_main.py
@@ -49,6 +49,17 @@ if clientID != -1:
         robot_joint_bounding_handles.append(bounding_handle)
         vrep.simxSetObjectParent(clientID,robot_joint_bounding_handles[joint],joint_handles[joint],False,vrep.simx_opmode_oneshot_wait)
 
+    # Kevin
+    wall_handles = []
+    wall_rows = 3
+    wall_cols = 3
+    for wall_row in range(wall_rows):
+        for wall_col in range(wall_cols):
+            count = wall_col*wall_row
+            errorCode, bounding_handle = vrep.simxCreateDummy(clientID, 0.10, None, vrep.simx_opmode_oneshot_wait)
+            wall_handles.append(bounding_handle)
+            vrep.simxSetObjectPosition(clientID,bounding_handle,-1,[0.01*count,0.01*count,0.01*count],vrep.simx_opmode_oneshot_wait)
+            print(wall_row, wall_col)
     # Get handles for all the movable collision detection dummies
     # Set number of obstacles
     NUM_OBSTACLES = 5
